1af21b90191833bbb7772313c58a83ee8bba1982
zchen9 <zhao.zchen9@gmail.com>
2016-05-11 21:51:27 +0800

    add some helpful tip

---
+++ credentials.js
@@ -0,0 +1,16 @@
+module.exports =  {
+  qiniu: {
+    ACCESS_KEY: '-L7HvCAanSlX7WOP-9w0UzRmzUddhgC2Sgmj__Km',
+    SECRET_KEY: 'y0DwGOrEO-1sAop0QGpLqI0zXPRGSUQgR8JB7Le1',
+    bucket: 'plotit',
+    uptoken_url: 'uptoken',
+    domain: 'http://7xrwkg.com1.z0.glb.clouddn.com/'
+  },
+  weibo: {
+    appkey: '1976616587'
+  },
+  wechat: {
+    aid: 'wxf22f393f544c7f24',
+    ase: '7a478172fdd6c82a80641e913cebeb0d'
+  }
+};
